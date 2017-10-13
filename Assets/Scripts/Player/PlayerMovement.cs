7c1cd6dde1183dfbb7b640a3ac34ab14895425f6
ronaldliu <ronaldl@mtu.edu>
2017-10-13 18:32:15 -0400

    Basic movement and turning controls

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour {
   Vector3 movement;
   Animator anim;
   Rigidbody playerRBody;
+  Camera playerCam;
+
   int floorMask;
   float camRayLength = 100f;
 
@@ -18,18 +20,25 @@ public class PlayerMovement : MonoBehaviour {
 
     anim = GetComponent<Animator>();
     playerRBody = GetComponent<Rigidbody>();
+
+    // Get the Main Character from the scene
+    playerCam = GameObject.Find("Main Camera").GetComponent<Camera>();
 	}
 
   // Runs all necessary updates for the player
   void FixedUpdate()
   {
+    // Get the horizontal and vertical directions for movement input
     float h = Input.GetAxis("Horizontal");
     float v = Input.GetAxis("Vertical");
 
+    // Move the player
     Move(h,v);
 
+    // Turn the player
     Turning();
 
+    // Animate the player base on direction of movement
     // Animating(h,v);
   }
 
@@ -46,10 +55,11 @@ public class PlayerMovement : MonoBehaviour {
   // Rotates the player base on the Camera position and Mouse position
   void Turning ()
   {
-    Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+    Ray camRay = playerCam.ScreenPointToRay (Input.mousePosition);
 
     RaycastHit floorHit;
 
+    // Get the position of the hit on the floor.
     if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask)) {
       Vector3 playerToMouse = floorHit.point - transform.position;
 
