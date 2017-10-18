c0e357ff6386d55f62eaa93315303d07e7cf8999
Doug <daferrib@mtu.edu>
2017-10-17 21:15:12 -0400

    Creating Enemy

---
+++ Assets/Scripts/Enemies/BasicEnemy.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    public float flashSpeed;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+
+    AudioSource enemyAudio;
+    EnemyMovement enemyMovement;
+    Renderer enemyRenderer;
+
+    bool isDead;
+    bool damaged;
+
+    // Use this for initialization
+    void Awake()
+    {
+        currentHealth = startingHealth;
+
+        enemyAudio = GetComponent<AudioSource>();
+        enemyMovement = GetComponent<EnemyMovement>();
+        enemyRenderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (damaged)
+        {
+            enemyRenderer.material.color = flashColor;
+        }
+        else
+        {
+            // Transition the color back to normal
+            enemyRenderer.material.color = Color.Lerp(enemyRenderer.material.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+
+        // Reset the damage
+        damaged = false;
+    }
+
+    public void applyDamage(int damage)
+    {
+        // Set damage to be true so we can show it
+        damaged = true;
+
+        // Apply the damage
+        currentHealth = currentHealth - damage;
+
+        // Check if the player is dead
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        // playerUse.DisableEffects();
+        // anim.SetTrigger("Die");
+
+        // playerAudio.clip = deathClip;
+        // playerAudio.Play();
+
+        enemyMovement.enabled = false;
+    }
+}
