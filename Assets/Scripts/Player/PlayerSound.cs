8d59849b73a88316ee8511285eb0a28aecc5b2e3
Ronald Liu <ronaldl@mtu.edu>
2017-10-22 12:17:07 -0400

    Adding an interface for audio to the player

---
+++ Assets/Scripts/Player/PlayerSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour {
+
+    public AudioClip playerHit;
+
+    private AudioSource source;
+
+	void Awake () {
+        source = GetComponent<AudioSource>();
+	}
+	
+    // Play sound when player gets hit
+	public void playHitSound()
+    {
+        source.PlayOneShot(playerHit, 0.5f);
+    }
+}
