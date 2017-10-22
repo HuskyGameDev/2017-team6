3521eec793bb1faf6f640626be87950b7d24e626
Ronald Liu <ronaldl@mtu.edu>
2017-10-22 12:26:48 -0400

    Adding Inventory Scripts

---
+++ Assets/Scripts/Player/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    class InventoryItem
+    {
+        
+    };
+    public int resources;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
