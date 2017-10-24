65387de182da25d3cf01ba1f294e0c7707a2139c
Ronald Liu <ronaldl@mtu.edu>
2017-10-24 13:07:54 -0400

    Adding Audio to the Game (#22)
    * Adding an interface for audio to the player
    
    * Adding weapon audio to the weapons

---
+++ Assets/Scripts/Player/PlayerSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour {
+
+    public AudioClip[] playerHit;
+
+    private AudioSource source;
+
+	void Awake () {
+        source = GetComponent<AudioSource>();
+	}
+	
+    // Play sound when player gets hit
+	public void playHitSound()
+    {
+        int hitSoundID = Mathf.CeilToInt(Random.Range(0, 4));
+        source.PlayOneShot(playerHit[hitSoundID], 0.2f);
+    }
+}
