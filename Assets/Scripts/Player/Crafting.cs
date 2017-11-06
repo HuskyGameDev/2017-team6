ab5553c9ae11e9db00416030292cc67d53b03757
ronaldliu <ronaldl@mtu.edu>
2017-11-05 21:45:15 -0500

    Implementation of Crafting w/ Inventory

---
+++ Assets/Scripts/Player/Crafting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crafting : MonoBehaviour
+{
+
+    private Inventory inventoryMngr;
+
+    // Use this for initialization
+    void Start()
+    {
+        inventoryMngr = GetComponent<Inventory>();
+    }
+
+    public void CraftItem()
+    {
+
+    }
+}
