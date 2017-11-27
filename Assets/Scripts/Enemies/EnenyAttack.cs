92e1dc812ba65d20c7078cbe7f6adf378784fb0b
Doug <daferrib@mtu.edu>
2017-11-27 11:15:20 -0500

    Added enemy attack script that checks for range, reload and attack (not functional yet)

---
+++ Assets/Scripts/Enemies/EnenyAttack.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnenyAttack : MonoBehaviour {
+
+    public float timeBetweenAttacks = 0.5f;     // The time in seconds between each attack.
+    public int attackDamage = 10;               // The amount of health taken away per attack.
+
+    Animator anim;                              // Reference to the animator component.
+    GameObject player;                          // Reference to the player GameObject.
+    PlayerManager playerHealth;                  // Reference to the player's health.
+    EnemyManager enemyHealth;                    // Reference to this enemy's health.
+    bool playerInRange;                         // Whether player is within the trigger collider and can be attacked.
+    float timer;                                // Timer for counting up to the next attack.
+
+
+    void Awake()
+    {
+        // Setting up the references.
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerManager>();
+        enemyHealth = GetComponent<EnemyManager>();
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If the entering collider is the player...
+        if (other.gameObject == player)
+        {
+            // ... the player is in range.
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // If the exiting collider is the player...
+        if (other.gameObject == player)
+        {
+            // ... the player is no longer in range.
+            playerInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        // Add the time since Update was last called to the timer.
+        timer += Time.deltaTime;
+
+        // If the timer exceeds the time between attacks, the player is in range and this enemy is alive...
+        if (timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+        {
+            // ... attack.
+            Attack();
+        }
+
+        // If the player has zero or less health...
+        if (playerHealth.currentHealth <= 0)
+        {
+            // ... tell the animator the player is dead.
+            anim.SetTrigger("PlayerDead");
+        }
+
+        
+        /*
+         * checks to see if relaod is nessisary
+        //if gun has no ammo, 
+        if ()
+        {
+            currentEquipped.Reloading();
+        }
+        */
+    }
+
+
+    void Attack()
+    {
+        // Reset the timer.
+        timer = 0f;
+
+        // If the player has health to lose...
+        if (playerHealth.currentHealth > 0)
+        {
+            /*
+             * Shoots if gun has ammo
+            //if gun has ammo, shoot
+            if ()
+            {
+                currentEquipped.Using();
+            }
+            */
+            
+        }
+    }
+
+}
