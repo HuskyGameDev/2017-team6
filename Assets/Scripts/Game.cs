755ee54ed13a86f0971d6adb71fddcdaf5252fee
ronaldliu <ronaldl@mtu.edu>
2017-12-09 17:33:42 -0500

    Gameflow Scripts and Functionality

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    GameObject playerObj;
+    SpawnPoint[] enemySpawners;
+
+	// Use this for initialization
+	void Start () {
+        playerObj = GameObject.Find("Player");
+        enemySpawners = GameObject.FindObjectsOfType<SpawnPoint>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (SpawnPoint sp in enemySpawners)
+        {
+            
+        }
+	}
+
+    // Next wave to spawn.
+    void SpawnNextWave()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+
+    public GameObject[] toSpawn;
+    int current;
+
+    // Use this for initialization
+    void Start() {
+        current = 0;
+    }
+
+    void SpawnNextObject()
+    {
+        GameObject spawnedObj = Instantiate(toSpawn[current]);
+        spawnedObj.transform.position = transform.position;
+    }
+}
