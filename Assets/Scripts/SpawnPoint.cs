9672d0e88cf108c1f8cc764b7bb302bec66e7305
Ronald Liu <ronaldl@mtu.edu>
2017-12-09 20:00:55 -0500

    Gameflow Scripts and Functionality (#56)
    * Gameflow Scripts and Functionality
    
    * Completed very basic gameflow script.
    
    * Adjusting some weapon attributes to make them feel better.

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+
+    // Spawn the "wave" stored in toSpawn
+    public void SpawnWave(GameObject[] toSpawn)
+    {
+        int i;
+        for (i = 0; i < toSpawn.Length; i++)
+        {
+            GameObject spawnedObj = Instantiate(toSpawn[i]);
+            spawnedObj.transform.position = transform.position;
+        }
+    }
+}
