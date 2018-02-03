6c24d439055bcd172d7d00d3f2fb541603f7d1f0
Michael Gryzwa <magryzwa@mtu.edu>
2018-02-03 12:28:14 -0500

    Added Basic Minimap Prefab

---
+++ Assets/Scripts/minimapscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minimapscript : MonoBehaviour {
+
+	public Transform player;
+
+	void Start(){
+		player = GameObject.Find ("Player").transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 newPosition = player.position;
+		newPosition.y = transform.position.y;
+		transform.position = newPosition;
+	}
+}
