ecba058e3291b9a6fe5df3133c7edb1728fcad38
Joseph Pietrzyk <joepietrzyk@gmail.com>
2018-02-11 13:10:43 -0500

    Made the WaveDisplay prefab panel and script FadingPanel.cs to allow a score display and game over text to be faded in and out.  Created the method UpdateWaveDisplay in GameUI.cs

---
+++ Assets/Scripts/FadingPanel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadingPanel : MonoBehaviour
+{
+	Image panel;
+	public float fadeInTime;
+	public float fadeOutTime;
+
+	void Start ()
+	{
+		bool hasFaded = true;
+		panel = GetComponent<Image> ();
+	}
+
+	public void FadeIn()
+	{
+		Debug.Log ("Fading panel in");
+		panel.CrossFadeAlpha (1f, fadeInTime, true);
+	}
+
+	public void FadeOut()
+	{
+		panel.CrossFadeAlpha (0f, fadeOutTime, true);
+	}
+}
+
