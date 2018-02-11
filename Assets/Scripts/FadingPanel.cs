bd5724750a72646101ec39a84c985e8dc30c1d4a
Joseph Pietrzyk <joepietrzyk@gmail.com>
2018-02-11 13:27:15 -0500

    The height of the window is now set in FadingPanel.cs in order to not clutter the UI prefab

--- Assets/Scripts/FadingPanel.cs
+++ Assets/Scripts/FadingPanel.cs
@@ -8,16 +8,19 @@ public class FadingPanel : MonoBehaviour
 	Image panel;
 	public float fadeInTime;
 	public float fadeOutTime;
+	public float windowHeight;
 
 	void Start ()
 	{
-		bool hasFaded = true;
 		panel = GetComponent<Image> ();
+		// The height is set in a script so that it doesn't clutter the UI prefab
+		GetComponent<RectTransform> ().SetSizeWithCurrentAnchors (RectTransform.Axis.Vertical, windowHeight);
+		// width is set to an arbitrarily high number to encapsulate the entire width of UI
+		GetComponent<RectTransform> ().SetSizeWithCurrentAnchors (RectTransform.Axis.Horizontal, 4000);
 	}
 
 	public void FadeIn()
 	{
-		Debug.Log ("Fading panel in");
 		panel.CrossFadeAlpha (1f, fadeInTime, true);
 	}
 
