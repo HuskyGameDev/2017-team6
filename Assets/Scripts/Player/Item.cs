317cb8ac89809d14ef36e53bd771b01c23422868
Jesse Moore <jessejm123@gmail.com>
2018-02-24 11:38:23 -0500

    ToolTip WIP2

--- Assets/Scripts/Player/Item.cs
+++ Assets/Scripts/Player/Item.cs
@@ -13,21 +13,37 @@ public abstract class Item : MonoBehaviour {
         public int wire;
     }
 
-    [Header("Base Item Info")]
-    public int max;
-    public int current;
+	public enum ItemType
+	{
+		Weapon, Melee, Throwable, Armor, Consumable
+	};
 
-    public string itemName;
-    public Sprite itemImg;
-    public enum Types
-    {
-        Weapon, Ammo, Consumable
-    };
+	public enum ItemTier
+	{
+		Standard, Unique, Rare, Epic, Legendary, Perfect, Hacked
+	};
+
+    [Header("Base Item Info")]
+    public int max;							// The max amount of instances that can fit in a stack
+    public int current;						// The current amount in this stack
+
+    public string itemName;					// Display name of the item
+	public ItemType itemType;				// The type of this item
+    public Sprite itemImg;					// Item icon image
+	//public Transform itemMesh;
+
+	[Header("Item Tier")]
+	public int level;						// A general measure of this item's worth
+	public ItemTier itemTier;				// The tier level dictated by level
 
     [Header("Item Cost")]
-    public ItemCost itemCost;
+    public ItemCost itemCost;				// The amount of each material needed to craft, if craftable
 
     public abstract void Using();
-
     public abstract void Reloading();
+
+	//public abstract ItemUpgrade[] GetUpgrades();
+	//public abstract void Upgrade(ItemUpgrade upgrade);
+
+	//public abstract void UpdateLevel();
 }
