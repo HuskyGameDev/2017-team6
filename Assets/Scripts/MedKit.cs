5e8d8aca78a814c482e1de58939c97241687f017
Jesse Moore <jessem@mtu.edu>
2018-02-24 16:00:49 -0500

    Medkit WIP

--- Assets/Scripts/MedKit.cs
+++ Assets/Scripts/MedKit.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MedKit : MonoBehaviour {
+public class MedKit : Item {
+
+	[Header("ItemStat Constants")]
+	protected List<ItemStat> stats_Medkit = new List<ItemStat> {
+		new ItemStat {name="Health Restored",field="hp",baseVal=50,canUpgrade=false,increaseOnLv=true,increment=1,limit=100}
+	};
+
+	float hp = 50;
 
 	// Use this for initialization
 	void Start () {
@@ -12,5 +19,24 @@ public class MedKit : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	// Inherited method for Using the weapon
+	public override void Using()
+	{
+		
+	}
+
+	// Inherited method for reloading
+	public override void Reloading()
+	{
+		
+	}
+
+	public override List<ItemStat> GetStats ()
+	{
+		List<ItemStat> stats = new List<ItemStat>();
+		stats.AddRange (stats_Medkit);
+		return stats;
 	}
 }
