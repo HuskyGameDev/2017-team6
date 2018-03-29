dc6b9741c56600baac2b22286822534ecf0d394f
Michael Gryzwa <magryzwa@mtu.edu>
2018-03-29 15:17:03 -0400

    added melee damage to toaster enemy so it actually does something

---
+++ Assets/Collision_Damage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision_Damage : MonoBehaviour {
+
+	public int Damage;
+
+	void Awake(){
+		Damage = 5;
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player") {
+			col.GetComponent<PlayerManager>().applyDamage(Damage);
+		}
+	}
+}
--- Assets/Scripts/Enemies/EnemyManager.cs
+++ Assets/Scripts/Enemies/EnemyManager.cs
@@ -113,4 +113,5 @@ public class EnemyManager : UnitManager {
 	{
 		enemyAttack.removeHeldItem ();
 	}
+		
 }
